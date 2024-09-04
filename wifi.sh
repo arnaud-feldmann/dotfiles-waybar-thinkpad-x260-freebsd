071e6794aa9745d820a1eb667d488b434b99193b
arnaud-feldmann <arnaud.feldmann@gmail.com>
2024-09-04 17:25:09 +0200

    correction mise en forme réseau

--- wifi.sh
+++ wifi.sh
@@ -1,6 +1,12 @@
 #/bin/sh
 
-RESEAU_ACCEDE=$(ifconfig wlan0 | grep ssid | awk '{print $2}')
-{ [ "$RESEAU_ACCEDE" = "\"\"" ] && echo "Déconnecté  "; } ||
-    { echo -e "$RESEAU_ACCEDE  "; }
+RESEAU_ACCEDE=$(ifconfig wlan0 | grep ssid | sed -E "s/.*ssid (.*) channel.*/\1/" | sed -E "s/^\"?([^\"]*)\"?$/\1/")
+
+if [ ${#RESEAU_ACCEDE} -gt 12 ]; then
+    RESEAU_ACCEDE=$(echo "$RESEAU_ACCEDE" | cut -c1-12)…
+elif [ -z "$RESEAU_ACCEDE" ]; then
+    RESEAU_ACCEDE="Déconnecté"
+fi
+
+echo -e "$RESEAU_ACCEDE  "
 
